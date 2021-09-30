7edb0f35f0ddd98a2838c8c2959718009d4ff149
Oskar Grunning <oskargrunning@gmail.com>
2021-09-30 16:37:19 +0200

    chore: run linting and formatting after update

--- src/color-scaper.js
+++ src/color-scaper.js
@@ -2,94 +2,93 @@
 //  ┃  ┃ ┃┃  ┃ ┃┣┳┛   ┗━┓┃  ┣┳┛┣━┫┣━┛┣╸ ┣┳┛
 //  ┗━╸┗━┛┗━╸┗━┛╹┗╸   ┗━┛┗━╸╹┗╸╹ ╹╹  ┗━╸╹┗╸
 
-import puppeteer from 'puppeteer'
+import puppeteer from 'puppeteer';
 
 import {
   convertToDefinitions,
   ensureUniqueDefinitions,
   sortDefinitions,
   writeToFile,
-} from './helpers'
-import { compose, flatten } from './utility'
+} from './helpers';
+import { compose, flatten } from './utility';
 
 const processColorhexa = async (browser) => {
-  const page = await browser.newPage()
+  const page = await browser.newPage();
 
-  await page.goto('https://www.colorhexa.com/color-names')
-  await page.waitForSelector('table.color-list')
+  await page.goto('https://www.colorhexa.com/color-names');
+  await page.waitForSelector('table.color-list');
 
   return page.evaluate(() => {
-    const tableBodyElement = document.querySelector('table.color-list > tbody')
-    const tableRowElements = tableBodyElement.querySelectorAll('tr')
+    const tableBodyElement = document.querySelector('table.color-list > tbody');
+    const tableRowElements = tableBodyElement.querySelectorAll('tr');
 
     return Array.from(tableRowElements).map((tableRowElement) => {
-      const [name, val] = tableRowElement.children
+      const [name, val] = tableRowElement.children;
 
       return {
         name: name.firstChild.textContent.trim(),
         val: val.firstChild.textContent.trim(),
-      }
-    })
-  })
-}
+      };
+    });
+  });
+};
 
 const processWikipedia = async (browser) => {
   const paths = [
     'List_of_colors:_A%E2%80%93F',
     'List_of_colors:_G%E2%80%93M',
     'List_of_colors:_N%E2%80%93Z',
-  ]
+  ];
 
   return Promise.all(
     paths.map(async (path) => {
-      const page = await browser.newPage()
+      const page = await browser.newPage();
 
-      await page.goto('https://en.wikipedia.org/wiki/' + path)
-      await page.waitForSelector('table.wikitable')
+      await page.goto('https://en.wikipedia.org/wiki/' + path);
+      await page.waitForSelector('table.wikitable');
 
       return page.evaluate(() => {
         const tableBodyElement = document.querySelector(
           'table.wikitable > tbody'
-        )
-        const tableRowElements = tableBodyElement.querySelectorAll('tr')
+        );
+        const tableRowElements = tableBodyElement.querySelectorAll('tr');
 
         return Array.from(tableRowElements).map((tableRowElement) => {
-          const [name, val] = tableRowElement.children
+          const [name, val] = tableRowElement.children;
 
           return {
             name: name.firstChild.textContent.trim(),
             val: val.textContent.trim(),
-          }
-        })
-      })
+          };
+        });
+      });
     })
-  )
-}
+  );
+};
 
 const scrapeSources = async (browser) => {
   const res = await Promise.all([
     processColorhexa(browser),
     processWikipedia(browser),
-  ])
+  ]);
 
   return compose(
     sortDefinitions,
     ensureUniqueDefinitions,
     convertToDefinitions,
     flatten
-  )(res)
-}
+  )(res);
+};
 
-// eslint-disable-next-line fp/no-nil
-;(async () => {
+(async () => {
   try {
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch();
 
-    writeToFile(await scrapeSources(browser))('../data/colors.json')
+    writeToFile(await scrapeSources(browser))('../data/colors.json');
 
-    await browser.close()
+    await browser.close();
   } catch (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   }
-})()
+})();
